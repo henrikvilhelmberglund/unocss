6c8bc4b5a61b06e1a8261d77e86b3bce2ab5baf2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-08-02 16:34:38 +0200

    chore: add experimentation file

---
+++ a.ts
@@ -0,0 +1,19 @@
+// import { splitCodeWithArbitraryVariants } from "@unocss/extractor-arbitrary-variants"
+// import { createGenerator, presetUno } from "unocss";
+
+// let myString = "[&_.recharts-cartesian-grid_line[stroke='#ccc']]:text-purple-500"
+
+// let newString = splitCodeWithArbitraryVariants(myString);
+
+// const uno = createGenerator({
+//   presets: [
+//     presetUno(),
+//   ],
+// })
+
+// const { css, matched } = await uno.generate(myString, { minify: true, preflights: false })
+
+// console.log(newString)
+// console.log(css)
+// console.log(matched)
+// console.log([...matched].join(' '));
